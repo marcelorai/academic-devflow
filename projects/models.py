f2b79e6bde12fc488088c26379f58f68d37b6bbf
Adriel <adriel.fsantos@outlook.com>
2022-10-19 22:38:46 -0300

    Implementa versão básica do modelo Projeto #26

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
 class Projeto(models.Model):
-  pass
+    descricao = models.TextField(blank=True, null=True)
+    dt_inicio = models.DateField()
+    dt_termino = models.DateField(blank=True, null=True)
+    nome = models.CharField(max_length=70)
+    situacao = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
