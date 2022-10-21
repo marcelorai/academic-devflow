600b1cc3f8af5363112c13d2482d4a6a27e122b0
Adriel <adriel.fsantos@outlook.com>
2022-10-21 10:41:51 -0300

    Implementa view de atualização de projeto #26

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import UpdateView
 
-# Create your views here.
+from projects.models import Projeto
+
+
+class ProjetoUpdateView(UpdateView):
+    model = Projeto
+    fields = '__all__'
+
+    def get_success_url(self):
+        return '/'
+
+
+update_view = ProjetoUpdateView.as_view()
