c49f5e1ce4858c27266f2d5d9a8a55de8fc868ef
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-10-25 15:38:34 -0300

    concertando a url #22

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from projects.views import update_view, postProject
+from projects.views import postProject
 
 app_name = 'projects'
 
 urlpatterns = [
-    path('<int:pk>/editar', update_view, name='update'),
-    path('/register', postProject, name='register')
+    path('register/', postProject)
 ]
