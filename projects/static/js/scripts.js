ec8f860ced7edadd9c22e2f3a7cd7b439f0dc206
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-11-10 20:07:27 -0300

    condificando o botão de filtrar projetos #27

--- projects/static/js/scripts.js
+++ projects/static/js/scripts.js
@@ -1,4 +1,17 @@
-
 const filterProjects = () => {
-    alert("Funcionou!")
+
+    const buttonFilterProject = document.getElementById('filter_project_button');
+    const formFilterProject = document.getElementById('filter_project_form');
+
+    if (buttonFilterProject.classList.contains('disabled')) {
+        buttonFilterProject.classList.remove('disabled')
+        buttonFilterProject.classList.add('activated')
+        formFilterProject.style.display = "block"
+
+    } else if(buttonFilterProject.classList.contains('activated')){
+        buttonFilterProject.classList.remove('activated')
+        buttonFilterProject.classList.add('disabled')
+        formFilterProject.style.display = "none"
+    }
+
 }
