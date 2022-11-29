21a9b38587774f438b79b290f00a90090e3df9be
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-11-29 14:01:36 -0300

    implementando a função de verificação das caixas de seleção #38

--- projects/static/js/scripts.js
+++ projects/static/js/scripts.js
@@ -15,3 +15,28 @@ const filterProjects = () => {
     }
 
 }
+
+const check_checkboxes = () => { 
+    const get_all_checkboxes = document.querySelectorAll('[name="codigo"]')
+    const get_delete_project_button = document.getElementById('delete_project_button')
+    const get_trash_icon = document.getElementById('trash_icon')
+
+    let cont = 0
+    get_all_checkboxes.forEach(element => {
+        if(element.checked){
+           cont += 1
+        }
+    }); 
+    
+    if(cont == 1){
+        get_delete_project_button.disabled = false
+        get_trash_icon.style.color = "red"
+    } else {
+        get_delete_project_button.disabled = true
+        get_trash_icon.style.color = "grey"
+    }
+}
+
+
+
+
