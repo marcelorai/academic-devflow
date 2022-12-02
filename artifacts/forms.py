505d90d1f717542bc4845b066f7baa6ae6e66690
Adriel <adriel.fsantos@outlook.com>
2022-12-02 16:15:23 -0300

    Adiciona campo projeto no ArtefatoCreateForm #39

--- artifacts/forms.py
+++ artifacts/forms.py
@@ -3,14 +3,14 @@ from django import forms
 from .models import Artefato
 
 
-
 class ArtefatoCreateForm(forms.ModelForm):
     class Meta:
         model = Artefato
-        fields = ['nome', 'descricao', 'situacao', 'data_entrega']
+        fields = ['projeto', 'nome', 'descricao', 'situacao', 'data_entrega']
         widgets = {
             'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'data_entrega': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'descricao': forms.Textarea(attrs={'class': 'form-control'}),
+            'projeto': forms.Select(attrs={'class': 'form-select'}),
             'situacao': forms.TextInput(attrs={'class': 'form-control'}),
-            'data_entrega': forms.DateInput(attrs={'type':'date', 'class': 'form-control'})
         }
