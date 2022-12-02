0ff2a15cf7c020124aa98074eb638d2799bd53a0
Adriel <adriel.fsantos@outlook.com>
2022-12-02 13:35:03 -0300

    Implementa modelo Artefato #39

--- artifacts/models.py
+++ artifacts/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 
 class Artefato(models.Model):
-    pass
+    descricao = models.TextField(
+        blank=True, null=True, verbose_name='descrição')
+    nome = models.CharField(max_length=70)
+    situacao = models.CharField(max_length=25)
+    data_entrega = models.DateField(verbose_name="data de entrega")
+
+    def __str__(self):
+        return self.nome
