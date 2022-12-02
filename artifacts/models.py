2b9475e817c4153acfb8dbf3db46c711f374d4e4
Adriel <adriel.fsantos@outlook.com>
2022-12-02 16:14:34 -0300

    Implementa atributo projeto no modelo Artefato #39

--- artifacts/models.py
+++ artifacts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from projects.models import Projeto
+
 
 class Artefato(models.Model):
     descricao = models.TextField(
@@ -7,6 +9,7 @@ class Artefato(models.Model):
     nome = models.CharField(max_length=70)
     situacao = models.CharField(max_length=25)
     data_entrega = models.DateField(verbose_name="data de entrega")
+    projeto = models.ForeignKey(Projeto, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
