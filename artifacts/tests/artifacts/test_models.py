aeca254f94b800f398ddc2c7cfe64fdba3f478f1
Adriel <adriel.fsantos@outlook.com>
2022-12-02 13:21:27 -0300

    Implementa test_objeto_criado_todos_campos #39
    Implementa teste sobre o modelo Artefato para verificar se um objeto é
    criado corretamente quando todos os campos, inclusive os opcionais, são
    passados.

---
+++ artifacts/tests/artifacts/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from artifacts.models import Artefato
+
+
+class ArtefatoModel(TestCase):
+    DESCRICAO = "Modelo de dados conceitual"
+    NOME = "Modelo de dados"
+    SITUACAO = "Entregue"
+    DATA_ENTREGA = '2022-12-12'
+
+    def test_objeto_criado_todos_campos(self):
+        """O modelo cria corretamente o objeto quando todos os atributos são fornecidos"""
+
+        a = Artefato.objects.create(descricao=self.DESCRICAO, nome=self.NOME,
+                                    situacao=self.SITUACAO, data_entrega=self.DATA_ENTREGA)
+        self.assertTrue(isinstance(a, Artefato))
+        self.assertEqual(a.nome, self.NOME)
