ef1bc7dbef14e4e70fbb15a6b6a954bc24fde106
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-12-05 19:03:41 -0300

    modificando a url para a view home de artefato #42

--- artifacts/urls.py
+++ artifacts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from artifacts.views import create_artifact_view, artifact_list, filter_artifact_by_name
+from artifacts.views import create_artifact_view, artifact_list, home_artifact
 
 app_name = 'artifacts'
 
 urlpatterns = [
-    path('', filter_artifact_by_name, name='home'),
+    path('', home_artifact, name='home'),
     path('registrar/', create_artifact_view, name='registrar'),
     path('listar/', artifact_list, name='listar'),
 ]
