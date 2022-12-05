4672bd670d364f5f9e63c5894c16d7326b9fa7a2
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-12-04 21:37:41 -0300

    criando as views para os respectivos testes #42

--- artifacts/views.py
+++ artifacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ArtefatoCreateForm
+from artifacts.models import Artefato
 
 
 def create_artifact_view(request):
@@ -10,3 +11,12 @@ def create_artifact_view(request):
             form.save()
             return redirect('projects:listar')
     return render(request, 'artifacts/novo-artefato.html', {'form': form})
+
+def artifact_list(request):
+    artifacts = Artefato.objects.all()
+    return render(request, 'artifacts/list_all_artifacts.html', {'artifacts': artifacts})
+
+def filter_artifact_by_name(request):
+    name = request.GET.get('name')
+    artifacts = Artefato.objects.filter(nome=name)
+    return render(request, 'artifacts/home_artifacts.html', {'artifacts': artifacts})
