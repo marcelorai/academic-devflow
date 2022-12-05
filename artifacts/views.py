c73dfd66b17b78dce46f4942e4bf5dbb80e6e193
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-12-05 19:03:11 -0300

    criando a view home de artefato e inserindo a busca dentro #42

--- artifacts/views.py
+++ artifacts/views.py
@@ -9,14 +9,19 @@ def create_artifact_view(request):
         form = ArtefatoCreateForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('projects:listar')
+            return redirect('artifacts:listar')
     return render(request, 'artifacts/novo-artefato.html', {'form': form})
 
 def artifact_list(request):
     artifacts = Artefato.objects.all()
     return render(request, 'artifacts/list_all_artifacts.html', {'artifacts': artifacts})
 
-def filter_artifact_by_name(request):
-    name = request.GET.get('nome')
-    artifacts = Artefato.objects.filter(nome=name)
-    return render(request, 'artifacts/home_artifacts.html', {'artifacts': artifacts})
+def home_artifact(request):
+
+    name = request.GET.get('nome', None)
+
+    if name:
+        artifacts = Artefato.objects.filter(nome__contains=name)
+        return render(request, 'artifacts/home_artifacts.html', {'artifacts': artifacts})
+    else:
+        return render(request, 'artifacts/home_artifacts.html')
