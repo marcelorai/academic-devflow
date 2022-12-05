a5c86c20a4c7daedddc9a933ecc4339b89901a7f
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-12-04 21:37:59 -0300

    criando as urls para as views #42

--- artifacts/urls.py
+++ artifacts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from artifacts.views import create_artifact_view
+from artifacts.views import create_artifact_view, artifact_list, filter_artifact_by_name
 
 app_name = 'artifacts'
 
 urlpatterns = [
+    path('', filter_artifact_by_name, name='home'),
     path('registrar/', create_artifact_view, name='registrar'),
+    path('listar/', artifact_list, name='listar'),
 ]
