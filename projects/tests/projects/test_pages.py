981d86e515c39680283116eb17cbcba0f09572c1
Adriel <adriel.fsantos@outlook.com>
2022-12-08 21:49:48 -0300

    Remove linha desnecessaria

--- projects/tests/projects/test_pages.py
+++ projects/tests/projects/test_pages.py
@@ -15,7 +15,7 @@ class CreateProjectPage(StaticLiveServerTestCase):
         self.url = reverse_lazy('projects:registrar')
 
     def test_preenche_todos_os_campos_e_submete_com_sucesso(self):
-        self.driver.get(f"{self.live_server_url}/projetos/registrar")
+        self.driver.get(self.url)
 
         # preenche os campos do formulario
         self.driver.find_element(By.NAME, 'nome').send_keys("Projeto 1")
