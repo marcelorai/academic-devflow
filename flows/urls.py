651f49f6a4ff9bbe8b32872bf81d6a57903c9966
Adriel <adriel.fsantos@outlook.com>
2022-12-18 15:48:35 -0300

    Adiciona rota de pagina de detalhes de fluxo #63

--- flows/urls.py
+++ flows/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import pagina_inicial_view, adicionar_fluxo_view
+from .views import pagina_inicial_view, adicionar_fluxo_view, detalhes_fluxo_view
 
 app_name = 'flows'
 
 urlpatterns = [
     path('', pagina_inicial_view, name='inicio'),
     path('adicionar', adicionar_fluxo_view, name='adicionar'),
+    path('<int:pk>', detalhes_fluxo_view, name='detalhes'),
 ]
