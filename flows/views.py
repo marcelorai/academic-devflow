a2ad2a619fc82787b8d9f050bf77ccc78b5fc647
Adriel <adriel.fsantos@outlook.com>
2022-12-18 17:05:31 -0300

    Adiciona view de adição de etapa #63

--- flows/views.py
+++ flows/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Fluxo
-from .forms import CriarFluxoForm
+from .forms import CriarFluxoForm, CriarEtapaForm
 
 
 def pagina_inicial_view(request):
@@ -21,3 +21,16 @@ def adicionar_fluxo_view(request):
 def detalhes_fluxo_view(request, pk):
     fluxo = get_object_or_404(Fluxo, id=pk)
     return render(request, 'flows/fluxo/detalhes.html', {'fluxo': fluxo})
+
+
+def adicionar_etapa_view(request, fluxo_pk):
+    fluxo = get_object_or_404(Fluxo, id=fluxo_pk)
+    form = CriarEtapaForm()
+    if request.method == 'POST':
+        form = CriarEtapaForm(request.POST)
+        if form.is_valid():
+            etapa = form.save(commit=False)
+            etapa.fluxo = fluxo
+            etapa.save()
+            return redirect('flows:detalhes', pk=fluxo.id)
+    return render(request, 'flows/etapa/adicionar.html', {'form': form, 'fluxo': fluxo})
