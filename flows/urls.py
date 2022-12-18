b08b33358fed5467a36b34746987f655c22797f8
Adriel <adriel.fsantos@outlook.com>
2022-12-18 17:05:41 -0300

    Adiciona rota de adição de etapa #63

--- flows/urls.py
+++ flows/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import pagina_inicial_view, adicionar_fluxo_view, detalhes_fluxo_view
+from .views import pagina_inicial_view, adicionar_fluxo_view, detalhes_fluxo_view, adicionar_etapa_view
 
 app_name = 'flows'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', pagina_inicial_view, name='inicio'),
     path('adicionar', adicionar_fluxo_view, name='adicionar'),
     path('<int:pk>', detalhes_fluxo_view, name='detalhes'),
+    path('<int:fluxo_pk>/adicionar', adicionar_etapa_view, name='adicionar_etapa'),
 ]
