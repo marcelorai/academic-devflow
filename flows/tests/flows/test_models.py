1397431d6ffdc2d7bb48ed5aedc640d2bacaebb9
Adriel <adriel.fsantos@outlook.com>
2022-12-18 13:55:57 -0300

    Atualiza teste de modelo Etapa com campo de fluxo #63

--- flows/tests/flows/test_models.py
+++ flows/tests/flows/test_models.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-from flows.models import Etapa
+from flows.models import Etapa, Fluxo
 from django.utils import timezone
 
 
 class EtapaModel(TestCase):
     def setUp(self):
+        self.fluxo = Fluxo.objects.create(nome="XP")
         self.nome = 'Planejamento'
         self.descricao = 'Pensando o funcionamento do projeto'
         self.data_inicio = timezone.now()
@@ -16,13 +17,13 @@ class EtapaModel(TestCase):
     def test_objeto_criado_todos_campos(self):
         """O modelo cria corretamente o objeto quando todos os atributos são fornecidos"""
         etapa = Etapa.objects.create(numero=self.numero, nome=self.nome, descricao=self.descricao, data_inicio=self.data_inicio,
-                                     data_finalizacao=self.data_finalizacao, ativa=self.ativa, gameficada=self.gameficada)
+                                     data_finalizacao=self.data_finalizacao, ativa=self.ativa, gameficada=self.gameficada, fluxo=self.fluxo)
         self.assertIsInstance(etapa, Etapa)
         self.assertEqual(self.nome, etapa.nome)
 
     def test_objeto_criado_campos_obrigatorios(self):
         """O modelo cria corretamente o objeto quando apenas os atributos obrigatórios são fornecidos"""
         etapa = Etapa.objects.create(numero=self.numero, nome=self.nome, data_inicio=self.data_inicio,
-                                     data_finalizacao=self.data_finalizacao, ativa=self.ativa, gameficada=self.gameficada)
+                                     data_finalizacao=self.data_finalizacao, ativa=self.ativa, gameficada=self.gameficada, fluxo=self.fluxo)
         self.assertIsInstance(etapa, Etapa)
         self.assertEqual(self.nome, etapa.nome)
