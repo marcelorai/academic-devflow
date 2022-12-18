cf8ea1e4fb1473358fd188dce06e1ab62b8f64a3
Adriel <adriel.fsantos@outlook.com>
2022-12-18 11:05:27 -0300

    Insere app flows em lista de apps instalados #63

--- academicDevflow/settings.py
+++ academicDevflow/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'projects.apps.ProjectsConfig',
     'artifacts.apps.ArtifactsConfig',
+    'flows.apps.FlowsConfig',
     'fontawesomefree'
 ]
 
