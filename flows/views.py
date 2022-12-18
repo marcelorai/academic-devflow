024d97e10da306d6fc72d0c10bc02972c65d7dfb
Adriel <adriel.fsantos@outlook.com>
2022-12-18 15:15:20 -0300

    Adiciona view para criação de fluxo #63

--- flows/views.py
+++ flows/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Fluxo
+from .forms import CriarFluxoForm
 
 
 def pagina_inicial_view(request):
     fluxos = Fluxo.objects.all()
     return render(request, 'flows/fluxo/home.html', {'fluxos': fluxos})
+
+
+def adicionar_fluxo_view(request):
+    form = CriarFluxoForm()
+    if request.method == 'POST':
+        form = CriarFluxoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('flows:inicio')
+    return render(request, 'flows/fluxo/adicionar.html', {'form': form})
