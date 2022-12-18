f03cb9bf58db850ced6d3750e9a5ff73682826f7
Adriel <adriel.fsantos@outlook.com>
2022-12-18 15:48:26 -0300

    Adiciona view de pagina de detalhes de fluxo #63

--- flows/views.py
+++ flows/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Fluxo
 from .forms import CriarFluxoForm
 
@@ -16,3 +16,8 @@ def adicionar_fluxo_view(request):
             form.save()
             return redirect('flows:inicio')
     return render(request, 'flows/fluxo/adicionar.html', {'form': form})
+
+
+def detalhes_fluxo_view(request, pk):
+    fluxo = get_object_or_404(Fluxo, id=pk)
+    return render(request, 'flows/fluxo/detalhes.html', {'fluxo': fluxo})
