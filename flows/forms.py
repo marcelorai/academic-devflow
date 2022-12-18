f86d59951f9a4b5100e99b6d39f521c1fc6d04e8
Adriel <adriel.fsantos@outlook.com>
2022-12-18 17:05:17 -0300

    Adiciona form de adição de etapa #63

--- flows/forms.py
+++ flows/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Fluxo
+
+from .models import Etapa, Fluxo
 
 
 class CriarFluxoForm(forms.ModelForm):
@@ -9,4 +10,27 @@ class CriarFluxoForm(forms.ModelForm):
         widgets = {
             'nome': forms.TextInput(attrs={'class': 'form-control'}),
             'descricao': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
+class CriarEtapaForm(forms.ModelForm):
+    class Meta:
+        model = Etapa
+        fields = ['nome', 'numero', 'descricao', 'data_inicio',
+                  'data_finalizacao', 'gameficada']
+        widgets = {
+            'numero': forms.NumberInput(attrs={'class': 'form-control'}),
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'data_inicio': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'data_finalizacao': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'gameficada': forms.CheckboxInput(attrs={'class': 'form-check-input', 'role': 'switch'})
+        }
+
+        labels = {
+            'gameficada': 'Usar gameficação'
+        }
+
+        help_texts = {
+            'numero': 'Posição da etapa dentro do fluxo'
         }
