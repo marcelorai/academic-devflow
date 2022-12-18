7040c2fd6b38ce5f2cd31e384b20f4ee3c10e38b
Adriel <adriel.fsantos@outlook.com>
2022-12-18 12:07:06 -0300

    Adiciona teste de modelo Etapa #63

--- flows/models.py
+++ flows/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from .validators import validate_date_is_today_or_after
+
+
+class Etapa(models.Model):
+    numero = models.PositiveIntegerField(verbose_name='número')
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(
+        max_length=400, verbose_name='descrição', blank=True, null=True)
+    data_inicio = models.DateField(verbose_name='data de início')
+    data_finalizacao = models.DateTimeField(
+        verbose_name='data de finalização', validators=[validate_date_is_today_or_after])
+    ativa = models.BooleanField(default=False)
+    gameficada = models.BooleanField(default=False)
+    # fluxo = foreign key para o fluxo a que a etapa pertence
+
+    def __str__(self):
+        return self.nome
---
+++ flows/tests/flows/test_models.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from flows.models import Etapa
+from django.utils import timezone
+
+
+class EtapaModel(TestCase):
+    def setUp(self):
+        self.nome = 'Planejamento'
+        self.descricao = 'Pensando o funcionamento do projeto'
+        self.data_inicio = timezone.now()
+        self.data_finalizacao = timezone.now()
+        self.gameficada = False
+        self.numero = 1
+        self.ativa = True
+
+    def test_objeto_criado_todos_campos(self):
+        """O modelo cria corretamente o objeto quando todos os atributos são fornecidos"""
+        etapa = Etapa.objects.create(numero=self.numero, nome=self.nome, descricao=self.descricao, data_inicio=self.data_inicio,
+                                     data_finalizacao=self.data_finalizacao, ativa=self.ativa, gameficada=self.gameficada)
+        self.assertIsInstance(etapa, Etapa)
+        self.assertEqual(self.nome, etapa.nome)
+
+    def test_objeto_criado_campos_obrigatorios(self):
+        """O modelo cria corretamente o objeto quando apenas os atributos obrigatórios são fornecidos"""
+        etapa = Etapa.objects.create(numero=self.numero, nome=self.nome, data_inicio=self.data_inicio,
+                                     data_finalizacao=self.data_finalizacao, ativa=self.ativa, gameficada=self.gameficada)
+        self.assertIsInstance(etapa, Etapa)
+        self.assertEqual(self.nome, etapa.nome)
